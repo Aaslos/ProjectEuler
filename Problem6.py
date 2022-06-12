43b640611a9c752d1f9374756d17aff71b0c5685
Aaslos <arthur.schrijver@gmail.com>
2022-06-12 13:45:42 +0200

    solved

--- Problem6.py
+++ Problem6.py
@@ -3,3 +3,18 @@ print("Problem 6")
 #The square of the sum of the first ten natural numbers is,
 #Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+def squarenum(num):
+    return num*num
+
+i = 1
+totalsquare = 0
+totalsum = 0
+while i < 101:
+    totalsquare += squarenum(i)
+    totalsum += i
+    i+=1
+
+print(squarenum(totalsum))
+print(totalsquare)
+print(squarenum(totalsum)-totalsquare)
